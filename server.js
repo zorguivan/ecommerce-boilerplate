140437876424c4b0b730d9b9f02b451a3de588b6
zorguivan <zorgui.oz@gmail.com>
2021-02-03 17:29:18 +0100

    port

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ app.get('/*', function(req, res) {
     res.sendFile('index.html', { root: path.join(__dirname, '/public') });
 });
 
-app.listen(5000, () => console.log("We're LIVE BOIS!!!"));
+app.listen(3000, () => console.log("We're LIVE BOIS!!!"));
